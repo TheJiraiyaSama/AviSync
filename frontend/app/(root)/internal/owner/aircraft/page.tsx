0da1bf5d11789546afacc6141d4703d530e1aecf
Sagar6250 <sagars6250@gmail.com>
2024-01-14 00:55:39 +0530

    Modal for Placing Bid

--- frontend/app/(root)/internal/owner/aircraft/page.tsx
+++ frontend/app/(root)/internal/owner/aircraft/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { CButton } from "@/components/custom";
+import CCard from "@/components/custom/CCard";
+import { FilterListIcon } from "@/components/icons";
+import { airplaneName } from "@/constants";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+    return (
+        <div>
+            <div className="mb-12 flex flex-row justify-between">
+                <p className="heading-1">Fancy something New?</p>
+                <CButton halfWidth icon={<FilterListIcon />}>
+                    Filter
+                </CButton>
+            </div>
+            <div className="flex flex-wrap gap-8 justify-center">
+                {airplaneName.map((name) => (
+                    <CCard key={name} label={name} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-export default page
+export default page;
--- frontend/components/custom/CCard.tsx
+++ frontend/components/custom/CCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
     Card,
@@ -7,14 +8,51 @@ import {
     CardHeader,
     CardTitle,
 } from "../ui/card";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { CButton } from ".";
+import CInput from "./Inputs/CInput";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+    bid: z.coerce.number(),
+});
 
 type CCardProps = {
     label: string;
 };
 
 const CCard = ({ label }: CCardProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            bid: 0,
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
     return (
         <Card className="flex w-[400px] h-[250px] bg-secondary text-primary rounded-xl">
             <div className="self-center basis-2/5">
@@ -28,9 +66,64 @@ const CCard = ({ label }: CCardProps) => {
                     <p>Current Bid: {}</p>
                 </CardContent>
                 <CardContent>
-                    <CButton halfWidth cVariant="accent">
-                        Place Bid
-                    </CButton>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <CButton halfWidth cVariant="accent">
+                                Place Bid
+                            </CButton>
+                        </DialogTrigger>
+                        <DialogContent className="bg-secondary text-primary fill-primary p-16 sm:min-w-[600px] justify-center">
+                            <DialogHeader>
+                                <DialogTitle>
+                                    <p className="heading-2 font-normal mb-12">
+                                        {label}
+                                    </p>
+                                </DialogTitle>
+                                <div className="flex flex-col gap-4">
+                                    <p>Serial Number: {}</p>
+                                    <p>Engine Number: {}</p>
+                                    <p>Manufacture Price: {}</p>
+                                    <p>Current Bid: {}</p>
+                                    <Form {...form}>
+                                        <form
+                                            onSubmit={form.handleSubmit(
+                                                onSubmit
+                                            )}
+                                        >
+                                            <FormField
+                                                control={form.control}
+                                                name="bid"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <CInput
+                                                            variant={"invert"}
+                                                            fieldSize={"half"}
+                                                            inputLabel="Your Bid"
+                                                        />
+                                                    </FormItem>
+                                                )}
+                                            />
+                                            <div className="flex justify-between mt-8">
+                                                <CButton
+                                                    type="submit"
+                                                    halfWidth
+                                                    cVariant="accent"
+                                                >
+                                                    Place Bid
+                                                </CButton>
+                                                <CButton
+                                                    halfWidth
+                                                    className="bg-primary text-secondary"
+                                                >
+                                                    View Logs
+                                                </CButton>
+                                            </div>
+                                        </form>
+                                    </Form>
+                                </div>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
                 </CardContent>
             </div>
             <CardFooter className="p-0 pr-2 basis-3/5">
